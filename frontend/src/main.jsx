60a43c762828ba2e6876377cf52db265291a8f37
sumitbansode360 <sumitbansode360@gmail.com>
2025-06-11 14:52:47 +0530

    login and register done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import NotFound from './pages/NotFound.jsx'
+
+const route = createBrowserRouter([
+
+  {
+    path : '/',
+    element : <App /> ,
+    errorElement : <NotFound />
+  },
+    {
+    path : '/login',
+    element : <Login />,
+    errorElement : <NotFound />
+  },
+  {
+    path : '/register',
+    element : <Register />,
+    errorElement : <NotFound />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route}/>
   </StrictMode>,
 )
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+import api from '../api'
+import '../style/style.css'
+import { Navigate, useNavigate, Link } from 'react-router-dom'
+
+function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const res = await api.post('api/token/', {
+      "username" : username,
+      "password" : password
+    }, { skipAuth: true })
+    if (res.status == 200){
+      const data = await res.data
+      localStorage.setItem('ACCESS_TOKEN', data.access)
+      localStorage.setItem('REFRESH_TOKEN', data.refresh)
+      navigate('/')
+    }
+   
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <div className="auth-container">
+      <div className="auth-card">
+          <div className="auth-header">
+              <h1>TechSpark</h1>
+              <p>Sign in to your account</p>
+          </div>
+          
+          <form className="auth-form" onSubmit={handleLogin} method='post'>
+              <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input 
+                    type="text" 
+                    id="username" 
+                    name="username" 
+                    value={username}
+                    onChange={(e)=>setUsername(e.target.value)}
+                    required />
+              </div>
+              
+              <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                    required />
+              </div>
+              
+              <button type="submit" className="btn-primary">Sign In</button>
+          </form>
+          
+          <div className="auth-footer">
+              <p>Don't have an account? <Link to={'/register'}>Sign up</Link></p>
+          </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Login 
